f2b966a23af8a00cf2671c402df75c9b73abede8
johanx22x <johanrodsa2210@gmail.com>
2022-11-12 22:48:31 -0600

    Interrups handling

--- src/kernel.c
+++ src/kernel.c
@@ -1,4 +1,5 @@
 #include "bootloader/bootservices.h"
+#include "io/interrupts.h"
 #include "print/printf/printf.h"
 #include "print/dbgprinter.h"
 #include "util/string.h"
@@ -15,17 +16,10 @@ void _start(void) {
     printf("Hi, I'm a kernel!\nBootloader: %s\nBootloader version: %s\n", get_bootloader_name(), get_bootloader_version());
 
     init_memory();
+    init_interrupts();
 
-    void * page = request_page();
-    printf("Allocated page at %p\n", page);
-
-    void * page2 = request_page();
-    printf("Allocated page at %p\n", page2);
-
-    free_page(page2);
-
-    void * page3 = request_page();
-    printf("Allocated page at %p\n", page3);
+    uint64_t * badptr = (uint64_t*)0xffffffffdeadb000;
+    *badptr = 0xdeadbeef;
 
     panic("Kernel looping...");
 }
